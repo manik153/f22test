fc047cf3a02b5661b9f2b824797ba49265a503a5
Manik Mudliyar <manikatdev@gmail.com>
2019-01-26 22:49:15 +0530

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_01_26_123819) do
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "link"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_123819) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
     t.string "reply_type"
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_123819) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
